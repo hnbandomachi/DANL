e5f172bf338f6483f8d717f6b28fc26be5f0971e
HNB <hnbandomachi@gmail.com>
2020-11-03 17:08:30 +0700

    gan tinh dong

--- code/matlab/duongday.m
+++ code/matlab/duongday.m
@@ -1,7 +1,7 @@
 classdef duongday
    properties
       nut = [0,0];
-      R; L; X; dai; I;
+      R; L; X; dai; I=0;
       bac;
    end
     
--- code/matlab/he_so_k2.m
+++ code/matlab/he_so_k2.m
@@ -23,20 +23,26 @@ Nodes(15).name = 15; Nodes(15).P = 8; Nodes(15).cosphi = 0.85; Nodes(15).Udm = 2
 
 
 % Nhap thong so duong day
-duongdays(1).nut = [1, 3]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(2).nut = [3, 4]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(3).nut = [4, 9]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(4).nut = [9, 10]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(5).nut = [9, 14]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(6).nut = [1, 2]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(7).nut = [2, 6]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(8).nut = [6, 5]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(9).nut = [6, 7]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(10).nut = [7, 11]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(11).nut = [5, 8]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(12).nut = [8, 12]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(13).nut = [8, 13]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
-duongdays(14).nut = [13, 15]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).L = 15;
+duongdays(1).nut = [1, 3]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).dai = 15;
+duongdays(2).nut = [3, 4]; duongdays(2).R = 0.01; duongdays(2).L = 0.02; duongdays(2).X = 0.015; duongdays(2).dai = 15;
+duongdays(3).nut = [4, 9]; duongdays(3).R = 0.01; duongdays(3).L = 0.02; duongdays(3).X = 0.015; duongdays(3).dai = 15;
+duongdays(4).nut = [9, 10]; duongdays(4).R = 0.01; duongdays(4).L = 0.02; duongdays(4).X = 0.015; duongdays(4).dai = 15;
+duongdays(5).nut = [9, 14]; duongdays(5).R = 0.01; duongdays(5).L = 0.02; duongdays(5).X = 0.015; duongdays(5).dai = 15;
+duongdays(6).nut = [1, 2]; duongdays(6).R = 0.01; duongdays(6).L = 0.02; duongdays(6).X = 0.015; duongdays(6).dai = 15;
+duongdays(7).nut = [2, 6]; duongdays(7).R = 0.01; duongdays(7).L = 0.02; duongdays(7).X = 0.015; duongdays(7).dai = 15;
+duongdays(8).nut = [6, 5]; duongdays(8).R = 0.01; duongdays(8).L = 0.02; duongdays(8).X = 0.015; duongdays(8).dai = 15;
+duongdays(9).nut = [6, 7]; duongdays(9).R = 0.01; duongdays(9).L = 0.02; duongdays(9).X = 0.015; duongdays(9).dai = 15;
+duongdays(10).nut = [7, 11]; duongdays(10).R = 0.01; duongdays(10).L = 0.02; duongdays(10).X = 0.015; duongdays(10).dai = 15;
+duongdays(11).nut = [5, 8]; duongdays(11).R = 0.01; duongdays(11).L = 0.02; duongdays(11).X = 0.015; duongdays(11).dai = 15;
+duongdays(12).nut = [8, 12]; duongdays(12).R = 0.01; duongdays(12).L = 0.02; duongdays(12).X = 0.015; duongdays(12).dai = 15;
+duongdays(13).nut = [8, 13]; duongdays(13).R = 0.01; duongdays(13).L = 0.02; duongdays(13).X = 0.015; duongdays(13).dai = 15;
+duongdays(14).nut = [13, 15]; duongdays(14).R = 0.01; duongdays(14).L = 0.02; duongdays(14).X = 0.015; duongdays(14).dai = 15;
+
+% Tinh Q I cua nut
+for nut = 1:n
+   Nodes(nut).Q = Nodes(nut).P/(1 + Nodes(nut).cosphi*Nodes(nut).cosphi);
+   Nodes(nut).Idm = Nodes(nut).P/(sqrt(3)*Nodes(nut).Udm*Nodes(nut).cosphi);
+end
 
 % Tinh nut trai va phai cua day
 nut_trai = [];
@@ -46,8 +52,7 @@ for i = 1 : n-1
     nut_phai(length(nut_phai)+1) = duongdays(i).nut(2); 
 end
 
-% Tinh bac 1 cua day va nut
-
+% Tinh bac 1 cua nut
 for nut = 1 : n
     warn = false;
     for day = 1 : n -1
@@ -60,7 +65,35 @@ for nut = 1 : n
     end
 end
 
+% Tinh bac 1 cua day
+for day = 1 : n-1
+   if Nodes(duongdays(day).nut(2)).bac == 1
+       duongdays(day).bac = 1;
+   end
+end
 
+% Tinh dong day bac 1
+for day = 1 : n-1
+   if duongdays(day).bac == 1
+       duongdays(day).I = Nodes(duongdays(day).nut(2)).Idm;     
+   end
+end
+
+
+% Tinh dong day
+for chay = 1:(n-1)*1
+   for day = 1:n-1
+       phai = duongdays(day).nut(2);
+       for i = 1:n-1
+          if phai == duongdays(i).nut(1)
+              if duongdays(i).I > 0
+                 duongdays(day).I = duongdays(day).I + duongdays(i).I; 
+              end
+          end
+       end
+      
+   end
+end
 
 
 
