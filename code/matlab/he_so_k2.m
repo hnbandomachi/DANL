c6e7d73bd89887b22908473cfed3b76fe993a718
HNB <hnbandomachi@gmail.com>
2020-11-28 10:42:20 +0700

    so lieu hop ly

--- code/matlab/he_so_k2.m
+++ code/matlab/he_so_k2.m
@@ -10,46 +10,46 @@ Udm_cao = 110; Udm_ha = 22; U_cao = Udm_cao; U_ha = Udm_ha; kT = U_cao/U_ha; Upa
 % Ma tran kiem chung A
 A = [];
 
-% Nhap thong so cua nut
-Nodes(1).name = 1; Nodes(1).P = 4; Nodes(1).cosphi = 0.85; Nodes(1).Udm = 22;  Nodes(1).PV = 2;
-Nodes(2).name = 2; Nodes(2).P = 6; Nodes(2).cosphi = 0.85; Nodes(2).Udm = 22;  Nodes(2).PV = 3;
-Nodes(3).name = 3; Nodes(3).P = 7; Nodes(3).cosphi = 0.85; Nodes(3).Udm = 22; Nodes(3).PV = 3;
-Nodes(4).name = 4; Nodes(4).P = 3; Nodes(4).cosphi = 0.85; Nodes(4).Udm = 22;  Nodes(4).PV = 2;
-Nodes(5).name = 5; Nodes(5).P = 8; Nodes(5).cosphi = 0.85; Nodes(5).Udm = 22; Nodes(5).PV = 2;
-Nodes(6).name = 6; Nodes(6).P = 9; Nodes(6).cosphi = 0.85; Nodes(6).Udm = 22;  Nodes(6).PV = 2;
-Nodes(7).name = 7; Nodes(7).P = 10; Nodes(7).cosphi = 0.85; Nodes(7).Udm = 22;  Nodes(7).PV = 2;
-Nodes(8).name = 8; Nodes(8).P = 5; Nodes(8).cosphi = 0.85; Nodes(8).Udm = 22;  Nodes(8).PV = 2;
-Nodes(9).name = 9; Nodes(9).P = 7; Nodes(9).cosphi = 0.85; Nodes(9).Udm = 22; Nodes(9).PV = 2;
-Nodes(10).name = 10; Nodes(10).P = 6; Nodes(10).cosphi = 0.85; Nodes(10).Udm = 22; Nodes(10).PV = 2;
-Nodes(11).name = 11; Nodes(11).P = 2; Nodes(11).cosphi = 0.85; Nodes(11).Udm = 22; Nodes(11).PV = 2;
-Nodes(12).name = 12; Nodes(12).P = 5; Nodes(12).cosphi = 0.85; Nodes(12).Udm = 22;  Nodes(12).PV = 2;
-Nodes(13).name = 13; Nodes(13).P = 7; Nodes(13).cosphi = 0.85; Nodes(13).Udm = 22;  Nodes(13).PV = 2;
-Nodes(14).name = 14; Nodes(14).P = 8; Nodes(14).cosphi = 0.85; Nodes(14).Udm = 22;  Nodes(14).PV = 2;
-Nodes(15).name = 15; Nodes(15).P = 8; Nodes(15).cosphi = 0.85; Nodes(15).Udm = 22; Nodes(15).PV = 2;
+% Nhap thong so cua nut P = 200 -300        PV = >100
+Nodes(1).name = 1; Nodes(1).P = 240; Nodes(1).cosphi = 0.85;        Nodes(1).Udm = 22;  Nodes(1).PV = 150;
+Nodes(2).name = 2; Nodes(2).P = 260; Nodes(2).cosphi = 0.85;        Nodes(2).Udm = 22;  Nodes(2).PV = 150;
+Nodes(3).name = 3; Nodes(3).P = 270; Nodes(3).cosphi = 0.85;        Nodes(3).Udm = 22;  Nodes(3).PV = 160;
+Nodes(4).name = 4; Nodes(4).P = 230; Nodes(4).cosphi = 0.85;        Nodes(4).Udm = 22;  Nodes(4).PV = 150;
+Nodes(5).name = 5; Nodes(5).P = 280; Nodes(5).cosphi = 0.85;        Nodes(5).Udm = 22;  Nodes(5).PV = 180;
+Nodes(6).name = 6; Nodes(6).P = 290; Nodes(6).cosphi = 0.85;        Nodes(6).Udm = 22;  Nodes(6).PV = 100;
+Nodes(7).name = 7; Nodes(7).P = 300; Nodes(7).cosphi = 0.85;        Nodes(7).Udm = 22;  Nodes(7).PV = 100;
+Nodes(8).name = 8; Nodes(8).P = 250; Nodes(8).cosphi = 0.85;        Nodes(8).Udm = 22;  Nodes(8).PV = 170;
+Nodes(9).name = 9; Nodes(9).P = 270; Nodes(9).cosphi = 0.85;        Nodes(9).Udm = 22;  Nodes(9).PV = 100;
+Nodes(10).name = 10; Nodes(10).P = 260; Nodes(10).cosphi = 0.85;    Nodes(10).Udm = 22; Nodes(10).PV = 170;
+Nodes(11).name = 11; Nodes(11).P = 220; Nodes(11).cosphi = 0.85;    Nodes(11).Udm = 22; Nodes(11).PV = 150;
+Nodes(12).name = 12; Nodes(12).P = 250; Nodes(12).cosphi = 0.85;    Nodes(12).Udm = 22; Nodes(12).PV = 170;
+Nodes(13).name = 13; Nodes(13).P = 270; Nodes(13).cosphi = 0.85;    Nodes(13).Udm = 22; Nodes(13).PV = 100;
+Nodes(14).name = 14; Nodes(14).P = 280; Nodes(14).cosphi = 0.85;    Nodes(14).Udm = 22; Nodes(14).PV = 100;
+Nodes(15).name = 15; Nodes(15).P = 280; Nodes(15).cosphi = 0.85;    Nodes(15).Udm = 22; Nodes(15).PV = 100;
 
 
 % Nhap thong so duong day
-duongdays(1).nut = [1, 3]; duongdays(1).R = 0.01; duongdays(1).L = 0.02; duongdays(1).X = 0.015; duongdays(1).dai = 15;
-duongdays(2).nut = [3, 4]; duongdays(2).R = 0.01; duongdays(2).L = 0.02; duongdays(2).X = 0.015; duongdays(2).dai = 15;
-duongdays(3).nut = [4, 9]; duongdays(3).R = 0.01; duongdays(3).L = 0.02; duongdays(3).X = 0.015; duongdays(3).dai = 15;
-duongdays(4).nut = [9, 10]; duongdays(4).R = 0.01; duongdays(4).L = 0.02; duongdays(4).X = 0.015; duongdays(4).dai = 15;
-duongdays(5).nut = [9, 14]; duongdays(5).R = 0.01; duongdays(5).L = 0.02; duongdays(5).X = 0.015; duongdays(5).dai = 15;
-duongdays(6).nut = [1, 2]; duongdays(6).R = 0.01; duongdays(6).L = 0.02; duongdays(6).X = 0.015; duongdays(6).dai = 15;
-duongdays(7).nut = [2, 6]; duongdays(7).R = 0.01; duongdays(7).L = 0.02; duongdays(7).X = 0.015; duongdays(7).dai = 15;
-duongdays(8).nut = [6, 5]; duongdays(8).R = 0.01; duongdays(8).L = 0.02; duongdays(8).X = 0.015; duongdays(8).dai = 15;
-duongdays(9).nut = [6, 7]; duongdays(9).R = 0.01; duongdays(9).L = 0.02; duongdays(9).X = 0.015; duongdays(9).dai = 15;
-duongdays(10).nut = [7, 11]; duongdays(10).R = 0.01; duongdays(10).L = 0.02; duongdays(10).X = 0.015; duongdays(10).dai = 15;
-duongdays(11).nut = [5, 8]; duongdays(11).R = 0.01; duongdays(11).L = 0.02; duongdays(11).X = 0.015; duongdays(11).dai = 15;
-duongdays(12).nut = [8, 12]; duongdays(12).R = 0.01; duongdays(12).L = 0.02; duongdays(12).X = 0.015; duongdays(12).dai = 15;
-duongdays(13).nut = [8, 13]; duongdays(13).R = 0.01; duongdays(13).L = 0.02; duongdays(13).X = 0.015; duongdays(13).dai = 15;
-duongdays(14).nut = [13, 15]; duongdays(14).R = 0.01; duongdays(14).L = 0.02; duongdays(14).X = 0.015; duongdays(14).dai = 15;
+duongdays(1).nut = [1, 3]; duongdays(1).R = 0.072; duongdays(1).L = 0.32; duongdays(1).X = 0.0; duongdays(1).dai = 1;
+duongdays(2).nut = [3, 4]; duongdays(2).R = 0.072; duongdays(2).L = 0.32; duongdays(2).X = 0.0; duongdays(2).dai = 1;
+duongdays(3).nut = [4, 9]; duongdays(3).R = 0.072; duongdays(3).L = 0.32; duongdays(3).X = 0.0; duongdays(3).dai = 1;
+duongdays(4).nut = [9, 10]; duongdays(4).R = 0.072; duongdays(4).L = 0.32; duongdays(4).X = 0.0; duongdays(4).dai = 1;
+duongdays(5).nut = [9, 14]; duongdays(5).R = 0.072; duongdays(5).L = 0.32; duongdays(5).X = 0.0; duongdays(5).dai = 1;
+duongdays(6).nut = [1, 2]; duongdays(6).R = 0.072; duongdays(6).L = 0.32; duongdays(6).X = 0.0; duongdays(6).dai = 1;
+duongdays(7).nut = [2, 6]; duongdays(7).R = 0.072; duongdays(7).L = 0.32; duongdays(7).X = 0.0; duongdays(7).dai = 1;
+duongdays(8).nut = [6, 5]; duongdays(8).R = 0.072; duongdays(8).L = 0.32; duongdays(8).X = 0.0; duongdays(8).dai = 1;
+duongdays(9).nut = [6, 7]; duongdays(9).R = 0.072; duongdays(9).L = 0.32; duongdays(9).X = 0.0; duongdays(9).dai = 1;
+duongdays(10).nut = [7, 11]; duongdays(10).R = 0.072; duongdays(10).L = 0.32; duongdays(10).X = 0.0; duongdays(10).dai = 1;
+duongdays(11).nut = [5, 8]; duongdays(11).R = 0.072; duongdays(11).L = 0.32; duongdays(11).X = 0.0; duongdays(11).dai = 1;
+duongdays(12).nut = [8, 12]; duongdays(12).R = 0.072; duongdays(12).L = 0.32; duongdays(12).X = 0.0; duongdays(12).dai = 1;
+duongdays(13).nut = [8, 13]; duongdays(13).R = 0.072; duongdays(13).L = 0.32; duongdays(13).X = 0.0; duongdays(13).dai = 1;
+duongdays(14).nut = [13, 15]; duongdays(14).R = 0.072; duongdays(14).L = 0.32; duongdays(14).X = 0.0; duongdays(14).dai = 1;
 
 row = 0;
 for dochieu = 0.1:0.1:1
     dochieu
     Nodes(5).Idm
     row = row + 1; col = 0;
-    for nT = 1:-1:-8        
+    for nT = 9:-1:-9        
         col = col + 1;
         % Tinh Q I cua nut
         for nut = 1:n
@@ -112,7 +112,7 @@ for dochieu = 0.1:0.1:1
         % Tinh sut ap roi tren tung doan day
         for day = 1 : n-1
         %    deltaU = duongdays(day).delta;
-           I = duongdays(day).I;
+           I = duongdays(day).I/1000;       % doi sang kA
            R = duongdays(day).R*duongdays(day).dai;
            zL = duongdays(day).L*duongdays(day).dai;
            zX = duongdays(day).X*duongdays(day).dai;
@@ -121,8 +121,8 @@ for dochieu = 0.1:0.1:1
 
         % Kiem tra va dieu chinh kT cua MBA 
         % neu nhu co nut sut ap vuot qua 5%
-        maxDrop = 100;
-        minDrop = 100;
+%         maxDrop = 100;
+%         minDrop = 100;
 
 
 %         nT = nT - 1;
@@ -152,13 +152,10 @@ for dochieu = 0.1:0.1:1
         for i = 1:n 
             if(maxDrop < abs((Nodes(i).U - Udm_ha)/Udm_ha)*100)
                 maxDrop = abs((Nodes(i).U - Udm_ha)/Udm_ha)*100;
-            end
-
-            if(minDrop > abs((Nodes(i).U - Udm_ha)/22)*100)
-                minDrop = abs((Nodes(i).U - Udm_ha)/22)*100;
-            end
+            end            
         end
-        if (maxDrop < 2)
+        
+        if (maxDrop < 5)
             A(row, col) = nT;
         else
             A(row, col) = 999;
@@ -169,9 +166,9 @@ for dochieu = 0.1:0.1:1
 end
 
 
+
+
 A
-
-
 
 
 
