1fde48fed1b84984ce5f87272e90157ce58ad4bd
HNB <hnbandomachi@gmail.com>
2020-12-23 10:05:00 +0700

    done report

--- code/matlab/he_so_k2.m
+++ code/matlab/he_so_k2.m
@@ -16,24 +16,22 @@ RT = P_N*Udm_cao*Udm_cao/Sdm/Sdm/1000/5;
 % Ma tran kiem chung ket_qua
 ket_qua = [];
 
-delPV = 2500;
-delQ = 900;
 % Nhap thong so cua nut P = 2000 -3000        PV = >100
-Nodes(1).name = 1; Nodes(1).Ptai = 3400; Nodes(1).Q = 130 + delQ;        Nodes(1).Udm = 22;  Nodes(1).PV = 150*2 + delPV;
-Nodes(2).name = 2; Nodes(2).Ptai = 3600; Nodes(2).Q = 190 + delQ;        Nodes(2).Udm = 22;  Nodes(2).PV = 150*2 + delPV;
-Nodes(3).name = 3; Nodes(3).Ptai = 3700; Nodes(3).Q = 140 + delQ;        Nodes(3).Udm = 22;  Nodes(3).PV = 160*2 + delPV;
-Nodes(4).name = 4; Nodes(4).Ptai = 3300; Nodes(4).Q = 120 + delQ;        Nodes(4).Udm = 22;  Nodes(4).PV = 150*2 + delPV;
-Nodes(5).name = 5; Nodes(5).Ptai = 3800; Nodes(5).Q = 100 + delQ;        Nodes(5).Udm = 22;  Nodes(5).PV = 180*2 + delPV;
-Nodes(6).name = 6; Nodes(6).Ptai = 3900; Nodes(6).Q = 160 + delQ;        Nodes(6).Udm = 22;  Nodes(6).PV = 100*2 + delPV;
-Nodes(7).name = 7; Nodes(7).Ptai = 3000; Nodes(7).Q = 120 + delQ;        Nodes(7).Udm = 22;  Nodes(7).PV = 100*2 + delPV;
-Nodes(8).name = 8; Nodes(8).Ptai = 3500; Nodes(8).Q = 130 + delQ;        Nodes(8).Udm = 22;  Nodes(8).PV = 170*2 + delPV;
-Nodes(9).name = 9; Nodes(9).Ptai = 3700; Nodes(9).Q = 150 + delQ;        Nodes(9).Udm = 22;  Nodes(9).PV = 100*2 + delPV;
-Nodes(10).name = 10; Nodes(10).Ptai = 3600; Nodes(10).Q = 130 + delQ;    Nodes(10).Udm = 22; Nodes(10).PV = 170*2 + delPV;
-Nodes(11).name = 11; Nodes(11).Ptai = 3200; Nodes(11).Q = 110 + delQ;    Nodes(11).Udm = 22; Nodes(11).PV = 150*2 + delPV;
-Nodes(12).name = 12; Nodes(12).Ptai = 3500; Nodes(12).Q = 140 + delQ;    Nodes(12).Udm = 22; Nodes(12).PV = 170*2 + delPV;
-Nodes(13).name = 13; Nodes(13).Ptai = 3700; Nodes(13).Q = 80 + delQ;    Nodes(13).Udm = 22; Nodes(13).PV = 100*2 + delPV;
-Nodes(14).name = 14; Nodes(14).Ptai = 3800; Nodes(14).Q = 100 + delQ;    Nodes(14).Udm = 22; Nodes(14).PV = 100*2 + delPV;
-Nodes(15).name = 15; Nodes(15).Ptai = 3800; Nodes(15).Q = 100 + delQ;    Nodes(15).Udm = 22; Nodes(15).PV = 100*2 + delPV;
+Nodes(1).name = 1; Nodes(1).Ptai = 3400; Nodes(1).Q = 1030;        Nodes(1).Udm = 22;  Nodes(1).PV =  2800;
+Nodes(2).name = 2; Nodes(2).Ptai = 3600; Nodes(2).Q = 1090;        Nodes(2).Udm = 22;  Nodes(2).PV = 2800;
+Nodes(3).name = 3; Nodes(3).Ptai = 3700; Nodes(3).Q = 1040;        Nodes(3).Udm = 22;  Nodes(3).PV = 2820;
+Nodes(4).name = 4; Nodes(4).Ptai = 3300; Nodes(4).Q = 1020;        Nodes(4).Udm = 22;  Nodes(4).PV = 2860;
+Nodes(5).name = 5; Nodes(5).Ptai = 3800; Nodes(5).Q = 1000;        Nodes(5).Udm = 22;  Nodes(5).PV = 2700;
+Nodes(6).name = 6; Nodes(6).Ptai = 3900; Nodes(6).Q = 1060;        Nodes(6).Udm = 22;  Nodes(6).PV = 2700;
+Nodes(7).name = 7; Nodes(7).Ptai = 3000; Nodes(7).Q = 1020;        Nodes(7).Udm = 22;  Nodes(7).PV = 2840;
+Nodes(8).name = 8; Nodes(8).Ptai = 3500; Nodes(8).Q = 1030;        Nodes(8).Udm = 22;  Nodes(8).PV = 2700;
+Nodes(9).name = 9; Nodes(9).Ptai = 3700; Nodes(9).Q = 1050;        Nodes(9).Udm = 22;  Nodes(9).PV = 2840;
+Nodes(10).name = 10; Nodes(10).Ptai = 3600; Nodes(10).Q = 1030;    Nodes(10).Udm = 22; Nodes(10).PV = 2800;
+Nodes(11).name = 11; Nodes(11).Ptai = 3200; Nodes(11).Q = 1010;    Nodes(11).Udm = 22; Nodes(11).PV = 2840;
+Nodes(12).name = 12; Nodes(12).Ptai = 3500; Nodes(12).Q = 1040;    Nodes(12).Udm = 22; Nodes(12).PV = 2700;
+Nodes(13).name = 13; Nodes(13).Ptai = 3700; Nodes(13).Q = 980;    Nodes(13).Udm = 22; Nodes(13).PV = 2700;
+Nodes(14).name = 14; Nodes(14).Ptai = 3800; Nodes(14).Q = 1000;    Nodes(14).Udm = 22; Nodes(14).PV = 2700;
+Nodes(15).name = 15; Nodes(15).Ptai = 3800; Nodes(15).Q = 1000;    Nodes(15).Udm = 22; Nodes(15).PV = 2700;
 
 
 % Nhap thong so duong day
@@ -52,14 +50,16 @@ duongdays(12).nut = [8, 12]; duongdays(12).R = 0.072; duongdays(12).L = 0.32; du
 duongdays(13).nut = [8, 13]; duongdays(13).R = 0.072; duongdays(13).L = 0.32; duongdays(13).X = 0.0; duongdays(13).dai = 2;
 duongdays(14).nut = [13, 15]; duongdays(14).R = 0.072; duongdays(14).L = 0.32; duongdays(14).X = 0.0; duongdays(14).dai = 2;
 
+U = [];
+
 row = 0;
 for dochieu = 0:0.1:1
     dochieu
 %     Pnut = Nodes(12).P
 %     cosphi = Nodes(12).cosphi
 %     dongday = duongdays(6).I
+    row = row + 1; col = 0;  
     
-    row = row + 1; col = 0;
     for nT = 9:-1:-9        
         col = col + 1;
         
@@ -193,6 +193,10 @@ for dochieu = 0:0.1:1
             ket_qua(row, col) = 0;
         end
         
+    end
+    
+    for kU = 1:n
+        U(kU,row) = Nodes(kU).U; 
     end
     
 end
